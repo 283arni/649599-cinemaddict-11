dc29f54004b6587061dd3fcdc462535fcfa0da4d
Aleksandr Arnautov <283arni@gmail.com>
2020-04-03 10:29:34 +0300

    module2-task1

---
+++ src/commponents/button-more.js
@@ -0,0 +1,5 @@
+export const createMoreMoviesButtonTamplate = () => {
+  return (
+    `<button class="films-list__show-more">Show more</button>`
+  );
+};
---
+++ src/commponents/content.js
@@ -0,0 +1,11 @@
+export const createContentTamplate = () => {
+  return (
+    `<section class="films">
+      <section class="films-list">
+        <h2 class="films-list__title visually-hidden">All moveisList. Upcoming</h2>
+
+        <div class="films-list__container"></div>
+      </section>
+    </section>`
+  );
+};
